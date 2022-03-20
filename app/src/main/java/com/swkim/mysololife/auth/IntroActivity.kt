60c03e3fc114a9b6c92266bd4b2332a9228b5e70
Seungwon <dnjs5397@naver.com>
2022-03-20 23:56:51 +0900

    회원가입 기능 추가

--- app/src/main/java/com/swkim/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/swkim/mysololife/auth/IntroActivity.kt
@@ -1,12 +1,23 @@
 package com.swkim.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.swkim.mysololife.R
+import com.swkim.mysololife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.joinBtn.setOnClickListener {
+            startActivity(Intent(this, JoinActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/swkim/mysololife/auth/JoinActivity.kt
@@ -0,0 +1,68 @@
+package com.swkim.mysololife.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.swkim.mysololife.MainActivity
+import com.swkim.mysololife.R
+import com.swkim.mysololife.databinding.ActivityJoinBinding
+
+class JoinActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityJoinBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.joinBtn.setOnClickListener {
+
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val passwordCheck = binding.passwordCheck.text.toString()
+
+            // 값이 비어있는지 확인
+            when {
+                email.isEmpty() -> {
+                    Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                password.isEmpty() -> {
+                    Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                passwordCheck.isEmpty() -> {
+                    Toast.makeText(this, "비밀번호 확인을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                password != passwordCheck -> {
+                    Toast.makeText(this, "동일한 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                password.length < 6 -> {
+                    Toast.makeText(this, "비밀번호를 6자리 이상 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "계정이 생성되었습니다.", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this, MainActivity::class.java))
+                                finish()
+                            } else {
+                                // If sign in fails, display a message to the user.
+
+                            }
+                        }
+                }
+            }
+        }
+
+
+
+    }
+}
